f3bef7481c570f1695748a3b7cef6ba2397f2462
AleksandrTatarenko <78167369+AleksandrTatarenko@users.noreply.github.com>
2021-12-08 18:16:17 +0200

    homework 6

--- task-06.js
+++ task-06.js
@@ -1,13 +1,13 @@
 const passEl = document.getElementById('validation-input');
 console.log(passEl);
-const lengthPassEl = passEl.dataset.length;
+const lengthPassEl = Number(passEl.dataset.length);
 console.log(lengthPassEl);
 passEl.addEventListener('focus', onFocusInput);
 passEl.addEventListener('blur', onBlurInput);
 function onFocusInput(event) {
 }
 function onBlurInput(event) {
-   if(event.currentTarget.value.length == lengthPassEl){
+   if(event.currentTarget.value.length === lengthPassEl){
       event.currentTarget.classList.remove('invalid');
       event.currentTarget.classList.add('valid');
    } else {
--- task-09.js
+++ task-09.js
@@ -3,11 +3,11 @@ const btnEl = document.querySelector('.change-color');
 const color = getRandomHexColor()
 
 btnEl.addEventListener('click', () => {
+    const color = getRandomHexColor()
     document.body.style.backgroundColor = color;
     spnEl.textContent = color;
+    console.log(color);
 })
-
-console.log(color);
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
